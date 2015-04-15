2e403c89669a6fb5f0ef8bce4aa9bf279772703e
fatuk <fatukk@gmail.com>
2015-04-15 21:54:13 +0300

    Fix coupon directive

--- app/directives/couponDirective.js
+++ app/directives/couponDirective.js
@@ -11,15 +11,13 @@ angular.module('couponDirective', [])
 			return {
 				restrict: 'E',
 				templateUrl: 'views/coupon/index.html',
+				scope: {},
 				link: function (scope, el, attr) {
 					var locationArray = attr.location.split(','),
 						left = $.trim(locationArray[0]),
 						top = $.trim(locationArray[1]);
 
 					scope.id = attr.id;
-
-					console.log(attr.id);
-
 
 					el.find('.js-coupon').css({
 						left: left,
