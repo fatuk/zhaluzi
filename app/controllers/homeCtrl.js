d845676dcd30a61324c63c29718ae1786896818b
fatuk <fatukk@gmail.com>
2015-04-15 22:19:17 +0300

    Add coupons messages array to test

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -4,7 +4,29 @@ angular.module('homeCtrl', ['ngDialog'])
 			'use strict';
 
 			$scope.collected = [];
-			$scope.currentCoupon = {};
+			$scope.currentCouponEl = {};
+			$scope.currentCouponId = '';
+			$scope.couponMessages = [{
+				id: 1,
+				title: 'Ты просто Шерлок! Ты нашел первый купон!',
+				description: 'Можешь оформить скидку 2% на жалюзи прямо сейчас. Для этого оставь свой email и мы отправим на него промо-код с инструкцией.'
+			}, {
+				id: 2,
+				title: 'Ура, второй купон!',
+				description: 'Можешь оформить скидку 4% на жалюзи прямо сейчас. Для этого оставь свой email и мы отправим на него промо-код с инструкцией.'
+			}, {
+				id: 3,
+				title: 'Третий купон у тебя в кармане',
+				description: 'Можешь оформить скидку 6% на жалюзи прямо сейчас. Для этого оставь свой email и мы отправим на него промо-код с инструкцией.'
+			}, {
+				id: 4,
+				title: 'Невероятно! Чертыре купона это серьезно!',
+				description: 'Можешь оформить скидку 8% на жалюзи прямо сейчас. Для этого оставь свой email и мы отправим на него промо-код с инструкцией.'
+			}, {
+				id: 5,
+				title: 'Ты нашел последний пятый купон!',
+				description: 'Можешь оформить скидку 10% на жалюзи прямо сейчас. Для этого оставь свой email и мы отправим на него промо-код с инструкцией.'
+			}];
 
 			$scope.init = function () {
 
@@ -21,8 +43,9 @@ angular.module('homeCtrl', ['ngDialog'])
 				ngDialog.open({
 					template: 'views/modals/coupon-1.html',
 					preCloseCallback: function () {
-						$scope.currentCoupon.fadeOut('slow');
-					}
+						$scope.currentCouponEl.fadeOut('slow');
+					},
+					scope: $scope
 				});
 			};
 
@@ -35,12 +58,13 @@ angular.module('homeCtrl', ['ngDialog'])
 			};
 
 			$scope.collect = function (id, e) {
-				$scope.currentCoupon = $(e.currentTarget);
+				$scope.currentCouponEl = $(e.currentTarget);
+				$scope.currentCouponId = id;
 				$scope.collected.push({
 					id: id
 				});
 
-				$scope.currentCoupon.find('.js-coupon').addClass('collected');
+				$scope.currentCouponEl.find('.js-coupon').addClass('collected');
 
 				$scope.openCollectModal();
 			};
