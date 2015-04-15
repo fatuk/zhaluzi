831bd96de379231aa0dff2ccb11860daa6ce3e5a
fatuk <fatukk@gmail.com>
2015-04-15 23:37:19 +0300

    Implement coupons gambling

--- app/controllers/homeCtrl.js
+++ app/controllers/homeCtrl.js
@@ -8,6 +8,7 @@ angular.module('homeCtrl', ['ngDialog'])
 			$scope.currentCouponId = '';
 			$scope.couponMessages = [];
 			$scope.coupons = [];
+			$scope.couponsOnStage = [];
 
 			$scope.init = function () {
 				$scope.getCouponMessages();
@@ -30,7 +31,7 @@ angular.module('homeCtrl', ['ngDialog'])
 					.then(function (res) {
 						// Success
 						$scope.coupons = res;
-						$log.log(res);
+						$scope.gambleCoupons();
 					}, function (err) {
 						// Error
 						$log.error(err);
@@ -38,7 +39,20 @@ angular.module('homeCtrl', ['ngDialog'])
 			};
 
 			$scope.random = function (min, max) {
-				return Math.random() * (max - min) + min;
+				return Math.floor(Math.random() * (max - min + 1)) + min;
+			};
+
+			$scope.gambleCoupons = function () {
+				var floorsCount = $scope.coupons.length;
+
+				for (var i = 0; i < floorsCount; i++) {
+					var max = $scope.coupons[i].coupons.length - 1,
+						rnd = $scope.random(0, max),
+						randomCoupon = $scope.coupons[i].coupons[rnd];
+
+					$scope.couponsOnStage.push(randomCoupon);
+					console.log(randomCoupon);
+				};
 			};
 
 			$scope.skrollr = function () {
